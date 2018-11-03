244c7210174cf151f9944a6bebad74b78e03ed07
karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-11-03 20:57:05 +0200

    mysql password

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ const connectionSettings = {
   host: 'db',
   user: 'root',
   database: 'db_1',
-  password: process.env.MYSQL_ROOT_PASSWORD,
+  password: MYSQL_ROOT_PASSWORD,
   namedPlaceholders: true,
 };
 
