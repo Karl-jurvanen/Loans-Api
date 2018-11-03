4a01f463a7301446a8bca1eda3f4580af7b301c1
karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-11-03 21:36:27 +0200

    add mysql password to backend env

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ const connectionSettings = {
   host: 'db',
   user: 'root',
   database: 'db_1',
-  password: MYSQL_ROOT_PASSWORD,
+  password: process.env.MYSQL_ROOT_PASSWORD,
   namedPlaceholders: true,
 };
 
