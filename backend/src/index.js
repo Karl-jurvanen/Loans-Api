11da1af6f191afab1241abf032357184b62a4f55
karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-11-06 15:44:07 +0200

    split middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@ import Url from 'url';
 import { connectionSettings } from './settings';
 import { initDB } from './fixtures';
 import { databaseReady } from './helpers';
+import { checkAccept, checkContent } from './middleware';
 
 // Initialize DB
 (async () => {
@@ -48,30 +49,6 @@ test.get(`${apiPath}/test`, async (ctx) => {
     ctx.throw(500, error);
   }
 });
-
-// Middleware for checking accept headers
-const checkAccept = async (ctx, next) => {
-  console.log('checkAccept');
-  // If client does not accept 'application/json' as response type, throw '406 Not Acceptable'
-  if (!ctx.accepts('application/json')) {
-    ctx.throw(406);
-  }
-  // Set the response content type
-  ctx.type = 'application/json; charset=utf-8';
-  // Move to next middleware
-  await next();
-};
-
-// Middleware for checking request body content
-const checkContent = async (ctx, next) => {
-  console.log('checkContent');
-  // Check that the request content type is 'application/json'
-  if (!ctx.is('application/json')) {
-    ctx.throw(415, 'Request must be application/json');
-  }
-  // Move to next middleware
-  await next();
-};
 
 // Define todos paths
 const todosPath = `${apiPath}/todos`;
