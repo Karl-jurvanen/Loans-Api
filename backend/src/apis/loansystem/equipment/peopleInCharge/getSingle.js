848883dd767d89ab2cc2f8e01175483433bb7afb
karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-11-23 23:25:33 +0200

    loansystem api get single person in charge of a device

---
+++ backend/src/apis/loansystem/equipment/peopleInCharge/getSingle.js
@@ -0,0 +1,43 @@
+import mysql from 'mysql2/promise';
+import { connectionSettings } from '../../../../settings';
+import { loanSystem, equipmentAdminPath } from '../../../constants';
+import { checkAccept } from '../../../../middleware';
+
+export default loanSystem.get(`${equipmentAdminPath}`, checkAccept, async (ctx) => {
+  const { id, adminId } = ctx.params;
+
+  if (isNaN(id) || id.includes('.')) {
+    ctx.throw(400, 'id must be an integer');
+  } else if (isNaN(adminId) || adminId.includes('.')) {
+    ctx.throw(400, 'admin id must be an integer');
+  }
+  try {
+    const conn = await mysql.createConnection(connectionSettings);
+
+    const [data] = await conn.execute(
+      `
+    SELECT 
+        henkilo.id,
+        henkilo.etunimi AS 'firstName',
+        henkilo.sukunimi AS 'lastName'
+    FROM
+        laite,
+        vastuuhenkilo,
+        henkilo
+    WHERE
+        laite.id = vastuuhenkilo.laite_id
+        AND henkilo.id = vastuuhenkilo.henkilo_id
+        AND henkilo.id = :adminId
+        AND laite.id = :id;
+            `,
+      { id, adminId },
+    );
+
+    // if sql query returns values, return the sql result,
+    // else set body to null so a 204 is sent
+    ctx.body = data[0];
+  } catch (error) {
+    console.error('Error occurred:', error);
+    ctx.throw(500, error);
+  }
+});
--- backend/src/apis/loansystem/equipment/peopleInCharge/index.js
+++ backend/src/apis/loansystem/equipment/peopleInCharge/index.js
@@ -1,4 +1,5 @@
 import { loanSystem } from '../../../constants';
 import getAll from './getAll';
+import getSingle from './getSingle';
 
 export default loanSystem;
