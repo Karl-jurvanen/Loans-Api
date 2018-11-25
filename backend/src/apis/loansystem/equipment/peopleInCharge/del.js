9c7d096311b7925ef91db67420d44c205a34c4b0
karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-11-25 14:49:08 +0200

    loansystem admins delete endpoint

---
+++ backend/src/apis/loansystem/equipment/peopleInCharge/del.js
@@ -0,0 +1,39 @@
+import mysql from 'mysql2/promise';
+import { connectionSettings } from '../../../../settings';
+import { loanSystem, equipmentAdminPath } from '../../../constants';
+
+// DELETE /resource/:id/admins/:adminId
+export default loanSystem.del(equipmentAdminPath, async (ctx) => {
+  const { id, adminId } = ctx.params;
+  console.log('.del id contains:', id);
+  console.log('.del adminId contains:', adminId);
+
+  if (isNaN(id) || id.includes('.')) {
+    ctx.throw(400, 'id must be an integer');
+  } else if (isNaN(adminId) || adminId.includes('.')) {
+    ctx.throw(400, 'adminId must be an integer');
+  }
+
+  try {
+    const conn = await mysql.createConnection(connectionSettings);
+    const [status] = await conn.execute(
+      `
+              DELETE FROM vastuuhenkilo
+              WHERE laite_id = :id 
+              AND henkilo_id = :adminId;
+            `,
+      { id, adminId },
+    );
+
+    if (status.affectedRows === 0) {
+      // The row did not exist, return '404 Not found'
+      ctx.status = 404;
+    } else {
+      // Return '204 No Content' status code for successful delete
+      ctx.status = 204;
+    }
+  } catch (error) {
+    console.error('Error occurred:', error);
+    ctx.throw(500, error);
+  }
+});
