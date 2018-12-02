4c7c467919985e6860ae21c85ba0f1b480c4b633
karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-12-02 20:53:41 +0200

    add CORS headers

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,12 +16,24 @@ const port = process.env.PORT || 9000;
 // Instantiate a Koa server
 const app = new Koa();
 
+// set CORS headers so the api endpoints can be accessed from application
+app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Access, Authorization',
+  );
+  await next();
+});
+
+// app.use(cors({ origin: false, credentials: true }));
 app.use(test.routes());
 app.use(test.allowedMethods());
 app.use(todos.routes());
 app.use(todos.allowedMethods());
 app.use(loanSystem.routes());
 app.use(loanSystem.allowedMethods());
+
 // Start the server and keep listening on port until stopped
 app.listen(port);
 
