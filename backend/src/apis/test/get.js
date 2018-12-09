4ac13c8f3244795c257bf9d54ee8324b942e40c8
karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-12-09 16:57:21 +0200

    fix test endpoint

--- backend/src/apis/test/get.js
+++ backend/src/apis/test/get.js
@@ -1,27 +1,10 @@
-import mysql from 'mysql2/promise';
-import { connectionSettings } from '../../settings';
 import { test, apiPath } from '../constants';
 import { checkAccept } from '../../middleware';
 
-export default test.get(`${apiPath}/test/:id`, checkAccept, async (ctx) => {
-  const { id } = ctx.params;
-  console.log('.get id contains:', id);
-
-  if (isNaN(id) || id.includes('.')) {
-    ctx.throw(400, 'id must be an integer');
-  }
+export default test.get(`${apiPath}/test/`, checkAccept, async (ctx) => {
   try {
-    const conn = await mysql.createConnection(connectionSettings);
-    // call GetDevices();
-    const [data] = await conn.execute(
-      `
-        call getDevice(:id);
-          `,
-      { id },
-    );
-
     // Return all todos
-    ctx.body = data[0][0];
+    ctx.body = { greeting: 'Hello World!' };
   } catch (error) {
     console.error('Error occurred:', error);
     ctx.throw(500, error);
