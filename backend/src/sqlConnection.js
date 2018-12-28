56020701617223ee74db2368bb7059c07be244e4
karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-12-28 13:00:42 +0200

    add hearthbeat to SQL connection

--- backend/src/sqlConnection.js
+++ backend/src/sqlConnection.js
@@ -1,5 +1,18 @@
 import mysql from 'mysql2/promise';
 import { connectionSettings } from './settings';
 
-const pool = mysql.createPool(connectionSettings);
+export const pool = mysql.createPool(connectionSettings);
 export const getConnection = () => pool.getConnection();
+
+/*
+The connection times out after a few hours of no connections,
+and after that trying to connect to pool results in an error.
+his function is to try to solve that by pinging it once every 30 minutes.
+*/
+async function keepAlive() {
+  const conn = await getConnection();
+  conn.ping();
+  conn.release();
+}
+
+setInterval(keepAlive, 1000 * 60 * 30);
